c4e20d03ea66215ddc6545af3def355344d00a5a
ulisesram98 <61959999+ulisesram98@users.noreply.github.com>
2020-10-25 17:24:23 -0600

    prueba 3

---
+++ Ventana de prueba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pagina_Inicial
+{
+    public partial class Ventana_de_prueba : Form
+    {
+        public Ventana_de_prueba()
+        {
+            InitializeComponent();
+        }
+    }
+}
